97d70cdf55aee923cafd60aca250ee12d88909a4
doviec <doviechitiz@gmail.com>
2020-01-22 14:44:14 +0200

    added Remarks

--- src/gameClient/dbConnector.java
+++ src/gameClient/dbConnector.java
@@ -11,8 +11,10 @@ public class dbConnector {
 	public static final String jdbcUrl="jdbc:mysql://db-mysql-ams3-67328-do-user-4468260-0.db.ondigitalocean.com:25060/oop?useUnicode=yes&characterEncoding=UTF-8&useSSL=false";
 	public static final String jdbcUser="student";
 	public static final String jdbcUserPassword="OOP2020student";
-
-
+/**
+ * this class returns a matrix which represents the data of the data base
+ * @return a matrix which each column represents a different valuable  
+ */
 	public static Object[][] GetData() {
 		ResultSet rs=null;
 		Object[][] data = new Object[50][6];
@@ -22,14 +24,11 @@ public class dbConnector {
 					DriverManager.getConnection(jdbcUrl, jdbcUser, jdbcUserPassword);
 			Statement statement = connection.createStatement();
 			String allCustomersQuery = "SELECT * FROM Logs WHERE UserID=321711061;";
-			//String allCustomersQuery = "SELECT * FROM Logs";
 			rs = statement.executeQuery(allCustomersQuery);
 			try {
 				int counter = 0;
-				//rs.
 				while(rs.next() && counter<50)
 				{
-					//LogObj = new Log...
 					data[counter][0] = rs.getInt("levelID");
 					data[counter][1] = rs.getInt("moves");
 					data[counter][2] = rs.getDate("time");
@@ -40,15 +39,11 @@ public class dbConnector {
 				}
 			}
 			catch (Exception e) {
-				// TODO: handle exception
 			}
-
 			rs.close();
 			statement.close();		
 			connection.close();
-
 		}
-
 		catch (SQLException sqle) {
 			System.out.println("SQLException: " + sqle.getMessage());
 			System.out.println("Vendor Error: " + sqle.getErrorCode());
@@ -58,6 +53,15 @@ public class dbConnector {
 		}
 		return data;
 	}
+	/**
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * @param id
+	 * @return
+	 */
 	public static int[] getNumbergames(int id)
 	{
 		ResultSet rs=null;
@@ -70,11 +74,9 @@ public class dbConnector {
 					DriverManager.getConnection(jdbcUrl, jdbcUser, jdbcUserPassword);
 			Statement statement = connection.createStatement();
 			String allCustomersQuery = "SELECT * FROM Logs WHERE UserID="+id+";";
-			//String allCustomersQuery = "SELECT * FROM Logs";
 			rs = statement.executeQuery(allCustomersQuery);
 			try {
 
-				//rs.
 				while(rs.next())
 				{
 					int temp = rs.getInt("levelID");
@@ -106,6 +108,14 @@ public class dbConnector {
 		ans[1] = max;
 		return ans;
 	}
+	/**
+	 * by a given id this method returns a matrix [12][4] which  the rows 
+	 * represents the levels and the columns represents the score to beat, 
+	 * the score that the user achieved, the maximum moves, and the moves 
+	 * the user reached. 
+	 * @param id
+	 * @return
+	 */
 	public static double[][] bestResult(int id)
 	{
 		ResultSet rs=null;
@@ -165,7 +175,14 @@ public class dbConnector {
 		}
 		return ans;
 	}
-
+/**
+ * by a given a matrix with the information about the moves and scores of the user 
+ * this method will return an array filled with the rank of each level in relation 
+ * towards the rest of the participants in this project. Each position in the array 
+ * represents the level.
+ * @param helpArr
+ * @return
+ */
 	public static int[] findRank(double[][] helpArr)
 	{
 		Hashtable<Integer, Hashtable<Integer, Integer>> hashIDs = new Hashtable<Integer, Hashtable<Integer,Integer>>();
@@ -191,25 +208,25 @@ public class dbConnector {
 				}
 			}
 			base+=";";
-//			String allCustomersQuery = "SELECT * FROM Logs WHERE "
-//					+ "(LevelID=0 AND score > "+helpArr[0][0]+" AND moves <= "+helpArr[0][3]+")"
-//					+ " OR (LevelID=1 AND score > "+helpArr[1][0]+" AND moves <= "+helpArr[1][3]+")"
-//					+ " OR (LevelID=3 AND score > "+helpArr[2][0]+" AND moves <= "+helpArr[2][3]+")"
-//					+ " OR (LevelID=5 AND score > "+helpArr[3][0]+" AND moves <= "+helpArr[3][3]+")" 
-//					+ " OR (LevelID=9 AND score > "+helpArr[4][0]+" AND moves <= "+helpArr[4][3]+")"
-//					+ " OR (LevelID=11 AND score > "+helpArr[5][0]+" AND moves <= "+helpArr[5][3]+")"
-//					+ " OR (LevelID=13 AND score > "+helpArr[6][0]+" AND moves <= "+helpArr[6][3]+")"
-//					+ " OR (LevelID=16 AND score > "+helpArr[7][0]+" AND moves <= "+helpArr[7][3]+")"
-//					+ " OR (LevelID=19 AND score > "+helpArr[8][0]+" AND moves <= "+helpArr[8][3]+")"
-//					+ " OR (LevelID=20 AND score > "+helpArr[9][0]+" AND moves <= "+helpArr[9][3]+")"
-//					+ " OR (LevelID=23 AND score > "+helpArr[11][0]+" AND moves <= "+helpArr[11][3]+");";
+			//			String allCustomersQuery = "SELECT * FROM Logs WHERE "
+			//					+ "(LevelID=0 AND score > "+helpArr[0][0]+" AND moves <= "+helpArr[0][3]+")"
+			//					+ " OR (LevelID=1 AND score > "+helpArr[1][0]+" AND moves <= "+helpArr[1][3]+")"
+			//					+ " OR (LevelID=3 AND score > "+helpArr[2][0]+" AND moves <= "+helpArr[2][3]+")"
+			//					+ " OR (LevelID=5 AND score > "+helpArr[3][0]+" AND moves <= "+helpArr[3][3]+")" 
+			//					+ " OR (LevelID=9 AND score > "+helpArr[4][0]+" AND moves <= "+helpArr[4][3]+")"
+			//					+ " OR (LevelID=11 AND score > "+helpArr[5][0]+" AND moves <= "+helpArr[5][3]+")"
+			//					+ " OR (LevelID=13 AND score > "+helpArr[6][0]+" AND moves <= "+helpArr[6][3]+")"
+			//					+ " OR (LevelID=16 AND score > "+helpArr[7][0]+" AND moves <= "+helpArr[7][3]+")"
+			//					+ " OR (LevelID=19 AND score > "+helpArr[8][0]+" AND moves <= "+helpArr[8][3]+")"
+			//					+ " OR (LevelID=20 AND score > "+helpArr[9][0]+" AND moves <= "+helpArr[9][3]+")"
+			//					+ " OR (LevelID=23 AND score > "+helpArr[11][0]+" AND moves <= "+helpArr[11][3]+");";
 			String allCustomersQuery = base;
 			//String allCustomersQuery = "SELECT * FROM Logs";
 			rs = statement.executeQuery(allCustomersQuery);
 			try {
 				while(rs.next())
 				{
-					
+
 					int LevelID = rs.getInt("levelID");
 					if(hashIDs.get(LevelID) == null)
 					{
@@ -219,10 +236,10 @@ public class dbConnector {
 					int userID = rs.getInt("UserID");
 					if(tempHashIdByLevelID.get(userID) == null)
 					{
-					ans[whichStageReverse(LevelID)]++;
-					tempHashIdByLevelID.put(userID, userID);
+						ans[whichStageReverse(LevelID)]++;
+						tempHashIdByLevelID.put(userID, userID);
 					}
-					
+
 				}
 			}
 			catch (Exception e) {
@@ -241,6 +258,12 @@ public class dbConnector {
 		}
 		return ans;
 	}
+	/**
+	 * – By a given case number this method helps us translate the case number to its 
+	 * max moves allowed for that specific case and returns the value.
+	 * @param i
+	 * @return
+	 */
 	private static double amountMovesOther(int i) {
 		switch (i) {
 		case 0 : 
@@ -269,6 +292,12 @@ public class dbConnector {
 			return -1;
 		}
 	}
+	/**
+	 * By a given case number this method helps us translate the case number to 
+	 * its minimum score allowed for that specific case and returns the score.
+	 * @param index
+	 * @return
+	 */
 	private static int amountScore(int index) {
 		switch (index) {
 		case 0 : 
@@ -297,6 +326,12 @@ public class dbConnector {
 			return -1;
 		}
 	}
+	/**
+	 * By a given level this method helps us translate the level to its max 
+	 * moves allowed for that specific level and returns the amount of moves.
+	 * @param index
+	 * @return
+	 */
 	private static int amountMoves(int index) {
 		switch (index) {
 		case 0 : 
@@ -325,6 +360,12 @@ public class dbConnector {
 			return -1;
 		}
 	}
+	/**
+	 * By a given level this method helps us translate the level to its 
+	 * case number which represents the given level and returns it.
+	 * @param index
+	 * @return
+	 */
 	private static int whichStageReverse(int index) {
 		switch (index) {
 		case 0 : 
@@ -353,6 +394,12 @@ public class dbConnector {
 			return -1;
 		}
 	}
+	/**
+	 * By a given a case number this method helps us translate the case number 
+	 * to its level number which represents the given case number and returns it.
+	 * @param index
+	 * @return
+	 */
 	private static int whichStage(int index) {
 		switch (index) {
 		case 0 : 
